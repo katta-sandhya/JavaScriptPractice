385533d20fd7a3dc09859fc81c9bb3992fd3c973
katta-sandhya <katta.sandhya9999@gmail.com>
2024-04-21 14:56:59 +0530

    Firstcommit

---
+++ day1/add.js
@@ -0,0 +1,8 @@
+let number1 = 5;
+let number2 = 10;
+
+// Add the two numbers together
+letD: sum = number1 + number2;
+
+// Output the result
+console.log("The sum of", number1, "and", number2, "is:", sum);
