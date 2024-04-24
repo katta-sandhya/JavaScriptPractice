2d3ea335f2bfc66f2267207de54f9ea9d8003211
katta-sandhya <katta.sandhya9999@gmail.com>
2024-04-24 23:21:54 +0530

    day5-datatypes

---
+++ day5/number.js
@@ -0,0 +1,6 @@
+let x;//undefined
+x=2;
+let y=3.4;
+console.log(x);
+console.log(x==y);
+console.log(typeof 3+"<br>"+typeof 3.4);
---
+++ day5/string.js
@@ -0,0 +1,17 @@
+let x="js";//string
+let y=5+6+"js";
+let z="js"+5+6;
+let myself={name:"sandhya",age:21};//object
+let courses=["c++","Java","js"];//array
+let d=new Date();//a date without arguements
+let e=new Date("june 2, 2002");
+console.log(typeof x);
+console.log(y);
+console.log(z);
+console.log(myself.name);
+console.log(courses[0]);
+console.log(d);
+console.log(e);
+
+
+
